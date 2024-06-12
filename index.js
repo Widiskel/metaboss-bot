f40d6be8875047d6a534e0bff1ac7ca651524acf
widiskel <waputra16@gmail.com>
2024-06-12 23:21:15 +0700

    fix (async) fix async await

--- index.js
+++ index.js
@@ -128,11 +128,11 @@ Colldown       : ${millisecondsToHoursAndMinutes(event.bossInfo.remain)}
 
         event.setUserData(data);
         event.setMission(data.mission);
+        await setTimeout(resolve, 3000);
       } else {
         reject(new Error("Received unexpected response" + data));
       }
     });
-    await setTimeout(resolve, 3000);
   });
 }
 
@@ -235,8 +235,8 @@ Colldown       : ${millisecondsToHoursAndMinutes(event.bossInfo.remain)}
       } else {
         console.log("Failed to start mining, Mining not unlocked");
       }
+      setTimeout(resolve, 1000);
     });
-    setTimeout(resolve, 1000);
   });
 }
 
@@ -280,7 +280,7 @@ Colldown       : ${millisecondsToHoursAndMinutes(event.bossInfo.remain)}
       const messages = JSON.parse(wsMsg.toString("utf8"));
       const rc = messages.code;
       const data = messages.data;
-      // console.log(messages);
+      console.log(messages);
 
       if (rc == 404) {
         resolve();
