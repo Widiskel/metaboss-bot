1fe1d9eee99c0497c734b83b0e3235da75d09d43
widiskel <waputra16@gmail.com>
2024-06-13 17:30:08 +0700

    mod (account template) add template for multiple account:

--- src/account_tmp.js
+++ src/account_tmp.js
@@ -1,15 +1,26 @@
-// const account = [
-//   {
-//     code: "X",
-//     type: "X",
-//     data: {
-//       id: "X",
-//       username: "X",
-//       hash: "X",
-//       timeAuth: "X",
-//       data: "X",
-//     },
-//   }, //account 1
-// ];
+const account = [
+  {
+    code: "X",
+    type: "X",
+    data: {
+      id: "X",
+      username: "X",
+      hash: "X",
+      timeAuth: "X",
+      data: "X",
+    },
+  }, //account 1
+  {
+    code: "X",
+    type: "X",
+    data: {
+      id: "X",
+      username: "X",
+      hash: "X",
+      timeAuth: "X",
+      data: "X",
+    },
+  }, //account 2
+];
 
 export { account };
