8929bd7ae44047458fbd775aa831897cd3b0522f
L Hakim <luqmanul.hakim@dwidasa.com>
2024-06-13 22:11:25 +0700

    nambah biar yahud

--- src/account_tmp.js
+++ src/account_tmp.js
@@ -1,26 +1,34 @@
 const account = [
-  {
-    code: "X",
-    type: "X",
-    data: {
-      id: "X",
-      username: "X",
-      hash: "X",
-      timeAuth: "X",
-      data: "X",
-    },
-  }, //account 1
-  {
-    code: "X",
-    type: "X",
-    data: {
-      id: "X",
-      username: "X",
-      hash: "X",
-      timeAuth: "X",
-      data: "X",
-    },
-  }, //account 2
+  jsonTransformer(), //account 1
+  jsonTransformer(), //account 2
 ];
+
+
+function jsonTransformer(inputJson) {
+  // Definisikan variabel yang mewakili kunci
+  const code = "code";
+  const type = "type";
+  const dataKey = "data";
+  const id = "id";
+  const username = "username";
+  const hash = "hash";
+  const timeAuth = "timeAuth";
+  const data = "data";
+
+  // Buat objek baru dengan kunci variabel
+  const transformedJson = {
+    [code]: inputJson[code],
+    [type]: inputJson[type],
+    [dataKey]: {
+      [id]: inputJson[dataKey][id],
+      [username]: inputJson[dataKey][username],
+      [hash]: inputJson[dataKey][hash],
+      [timeAuth]: inputJson[dataKey][timeAuth],
+      [data]: inputJson[dataKey][data]
+    }
+  };
+
+  return transformedJson;
+}
 
 export { account };
