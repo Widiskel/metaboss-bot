70787c736c4cf764b92cf05ce3428c2a4fd3caac
widiskel <waputra16@gmail.com>
2024-06-15 21:05:05 +0700

    fix (missions) bypass telegram missions

--- index.js
+++ index.js
@@ -330,7 +330,7 @@ Colldown       : ${millisecondsToHoursAndMinutes(event.bossInfo.remain)}
       });
 
       const unCompleteMissions = event.missions.filter(
-        (item) => !item.completed && item.id != 6
+        (item) => !item.completed && item.id != 6 && item.id != 4
       );
       const unCompleteMissionsIds = unCompleteMissions.map((item) => item.id);
       for (const missionId of unCompleteMissionsIds) {
